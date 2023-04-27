d379ee68f4be3567186819f700985d051632388a
Shashank-2021 <workingwithshashank@gmail.com>
2023-04-27 17:45:26 +0530

    Adds todo using Go button - Uses states for current and all todos

--- src/App.js
+++ src/App.js
@@ -1,20 +1,39 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todos, setTodos] = useState([]);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (todo !== "") {
+      setTodos([{ id: `${todo}-${Date.now()}`, todo }, ...todos]);
+    }
+  };
+
   return (
     <div className="App">
       <div className="container">
         <h1>Todo List App</h1>
         <form className="todoForm">
-          <input type="text" className="input" />
-          <button>Go</button>
+          <input
+            type="text"
+            className="input"
+            onChange={(e) => setTodo(e.target.value)}
+          />
+          <button onClick={(e) => handleSubmit(e)}>Go</button>
         </form>
         <ul className="allTodos">
-          <li className="singleTodo">
-            <span className="todoText">Learn React</span>
-            <button>Edit</button>
-            <button>Delete</button>
-          </li>
+          {todos.map((t) => {
+            return (
+              <li className="singleTodo">
+                <span className="todoText">{t.todo}</span>
+                <button>Edit</button>
+                <button>Delete</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </div>
